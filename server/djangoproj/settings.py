1b8a3831c777f191b14bd3fa29638082a9237775
Etziony Wanniang <etziowan@gmail.com>
2024-12-15 09:22:15 -0500

    modify setting.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,7 +28,11 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'https://etzionywanni-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
+ALLOWED_HOSTS = [
+    'etzionywanni-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+    '127.0.0.1',
+    'localhost',
+]
 
 CSRF_TRUSTED_ORIGINS = ['https://etzionywanni-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
 
